d90d0343b0fc844f7dfb135317f549519c05742a
5jyan <saulpark12@gmail.com>
2025-08-13 22:09:04 +0900

    feat: set up netlify

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -7,7 +8,18 @@ const config = getDefaultConfig(__dirname);
 config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
 config.resolver.alias = {
   ...config.resolver.alias,
-  'react-native$': 'react-native-web'
+  'react-native$': 'react-native-web',
+  '@/components': path.resolve(__dirname, 'components'),
+  '@/src/components': path.resolve(__dirname, 'src/components'),
+  '@/hooks': path.resolve(__dirname, 'hooks'),
+  '@/src/hooks': path.resolve(__dirname, 'src/hooks'),
+  '@/services': path.resolve(__dirname, 'src/services'),
+  '@/stores': path.resolve(__dirname, 'src/stores'),
+  '@/lib': path.resolve(__dirname, 'src/lib'),
+  '@/types': path.resolve(__dirname, 'src/types'),
+  '@/constants': path.resolve(__dirname, 'constants'),
+  '@/contexts': path.resolve(__dirname, 'src/contexts'),
+  '@/assets': path.resolve(__dirname, 'assets')
 };
 
 // Web-specific transformer settings
